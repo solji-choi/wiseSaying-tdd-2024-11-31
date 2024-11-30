f49f575c407cd3592fa7c9882de69311cb87c2dd
solji-choi <solji.choi89@gmail.com>
2024-12-01 03:45:44 +0900

    명언앱 시작 테스트(r)

--- src/test/java/com/ll/AppTest.java
+++ src/test/java/com/ll/AppTest.java
@@ -1,4 +1,38 @@
 package com.ll;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class AppTest {
+    public static String run(String input) {
+        input = input.stripIndent().trim();
+
+        Scanner scanner = TestUtil.genScanner(input);
+
+        App app = new App(scanner);
+        app.run();
+
+        ByteArrayOutputStream outputStream = TestUtil.setOutToByteArray();
+
+        scanner.close();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        String output = outputStream.toString();
+
+        return output;
+    }
+
+    @Test
+    @DisplayName("== 명언 앱 ==")
+    public void t1() {
+        String output = AppTest.run("");
+
+        assertThat(output).contains("== 명언 앱 ==");
+    }
 }

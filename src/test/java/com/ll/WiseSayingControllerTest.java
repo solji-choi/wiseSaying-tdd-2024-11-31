78409277e894a100f3bf3d118cd6a9a3ca5c1184
solji-choi <solji.choi89@gmail.com>
2024-12-01 14:04:49 +0900

    목록 기능 추가(r)

--- src/test/java/com/ll/WiseSayingControllerTest.java
+++ src/test/java/com/ll/WiseSayingControllerTest.java
@@ -55,4 +55,24 @@ public class WiseSayingControllerTest {
                 .contains("작가 : ")
                 .contains("2번 명언이 등록되었습니다.");
     }
+
+    @Test
+    @DisplayName("목록 기능 추가")
+    public void t4() {
+        String output = AppTest.run("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                등록
+                과거에 집착하지 마라.
+                작자미상
+                목록
+                """);
+
+        assertThat(output)
+                .contains("번호 / 작가 / 명언")
+                .contains("---------------------- ")
+                .contains("2 / 작자미상 / 과거에 집착하지 마라.")
+                .contains("1 / 작자미상 / 현재를 사랑하라.");
+    }
 }
